fafb4b9d7d7504dd2028e508617f8e3eaed20ace
Seifkhaled1010 <seifkhaled1917@gmail.com>
2024-09-19 15:20:29 +0300

    Add project files.

---
+++ OOP@/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_
+{
+    public class Employee
+    {
+        public static double TAX = 0.03;
+
+        public string FNAME;
+        public string LNAME;
+        public double Wage;
+        public double LoggedHours;
+    }
+}
---
+++ OOP@/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace OOP_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee[] emps = new Employee[2];
+
+            Console.Write("TAX: ");
+            Employee.TAX = Convert.ToDouble(Console.ReadLine());
+
+            Employee e1 = new Employee();
+            Console.WriteLine("First Employee");
+            Console.Write("First Name:");
+            e1.FNAME = Console.ReadLine();
+
+            Console.Write("Last Name:");
+            e1.LNAME = Console.ReadLine();
+
+            Console.Write("Wage: ");
+            e1.Wage = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("LoggedHours: ");
+            e1.LoggedHours = Convert.ToDouble(Console.ReadLine());
+
+            emps[0] = e1;
+
+            Console.WriteLine("\nSecond Employee");
+
+            Employee e2 = new Employee();
+
+            Console.Write("First Name: ");
+            e2.FNAME = Console.ReadLine();
+
+            Console.Write("Last Name: ");
+            e2.LNAME = Console.ReadLine();
+
+            Console.Write("Wage: ");
+            e2.Wage = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("LoggedHours: ");
+            e2.LoggedHours = Convert.ToDouble(Console.ReadLine());
+
+            emps[1] = e2;
+
+            foreach (var emp in emps)
+            {
+                var salary = emp.Wage * emp.LoggedHours;
+                var taxAmount = salary * Employee.TAX;
+                var netSalary = salary - taxAmount;
+
+                Console.WriteLine($"\nFirst Name: {emp.FNAME}");
+                Console.WriteLine($"Last Name: {emp.LNAME}");
+                Console.WriteLine($"Wage: {emp.Wage}");
+                Console.WriteLine($"LoggedHours: {emp.LoggedHours}");
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine($"Salary: ${salary}");
+                Console.WriteLine($"Deductable Tax ({Employee.TAX * 100}%), Amount: ${taxAmount}");
+                Console.WriteLine($"Net Salary: {netSalary}\n");
+            }
+            Console.Read();
+        }
+    }
+}
